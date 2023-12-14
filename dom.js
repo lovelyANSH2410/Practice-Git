5dadc9c0a33cef48f8033c16c7a6d1164304f877
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2023-12-14 14:14:44 +0530

    added dom.js

---
+++ dom.js
@@ -0,0 +1,8 @@
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementsByClassName('title');
+console.log(items);
+items[0].style.fontWeight = 'bold';
+items[0].style.color = '#008000';
+//items.textContent.style.color = 'solid 3px #008000';
