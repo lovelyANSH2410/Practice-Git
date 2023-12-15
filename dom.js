8844f4ded2df380b03c8d809bf63ce18a0e791da
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2023-12-15 09:15:17 +0530

    using getElementByTagName

--- dom.js
+++ dom.js
@@ -1,8 +1,8 @@
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
+// var header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px #000';
 
-var items = document.getElementsByClassName('title');
-console.log(items);
+// var items = document.getElementsByClassName('title');
+// console.log(items);
 items[0].style.fontWeight = 'bold';
 items[0].style.color = '#008000';
 //items.textContent.style.color = 'solid 3px #008000';
@@ -19,4 +19,16 @@ item[2].style.backgroundColor = 'green';
 
 for(var i = 0; i<item.length; i++){
     item[i].style.fontWeight = 'bold';
+}
+
+//get element by tag name
+
+var li = document.getElementsByTagName('li');
+li[2].style.backgroundColor = 'green';
+
+error
+item.style.fontWeight = 'bold';
+
+for(var i = 0; i<li.length; i++){
+    li[i].style.fontWeight = 'bold';
 }
