a06ccfee7375095d0a693e2032162b6c24938ba8
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2023-12-15 09:31:30 +0530

    querySelector

--- dom.js
+++ dom.js
@@ -3,32 +3,40 @@
 
 // var items = document.getElementsByClassName('title');
 // console.log(items);
-items[0].style.fontWeight = 'bold';
-items[0].style.color = '#008000';
-//items.textContent.style.color = 'solid 3px #008000';
+// items[0].style.fontWeight = 'bold';
+// items[0].style.color = '#008000';
+// //items.textContent.style.color = 'solid 3px #008000';
 
 
 //using getelementbyclassname
 
-var item = document.getElementsByClassName('list-group-item');
-console.log(item);
-item[2].style.backgroundColor = 'green';
+// var item = document.getElementsByClassName('list-group-item');
+// console.log(item);
+// item[2].style.backgroundColor = 'green';
 
-//error
-//item.style.fontWeight = 'bold';
+// //error
+// //item.style.fontWeight = 'bold';
 
-for(var i = 0; i<item.length; i++){
-    item[i].style.fontWeight = 'bold';
-}
+// for(var i = 0; i<item.length; i++){
+//     item[i].style.fontWeight = 'bold';
+// }
 
-//get element by tag name
+// //get element by tag name
 
-var li = document.getElementsByTagName('li');
-li[2].style.backgroundColor = 'green';
+// var li = document.getElementsByTagName('li');
+// li[2].style.backgroundColor = 'green';
 
-error
-item.style.fontWeight = 'bold';
+// error
+// item.style.fontWeight = 'bold';
 
-for(var i = 0; i<li.length; i++){
-    li[i].style.fontWeight = 'bold';
-}
+// for(var i = 0; i<li.length; i++){
+//     li[i].style.fontWeight = 'bold';
+// }
+
+//query selector
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.color = 'white';
